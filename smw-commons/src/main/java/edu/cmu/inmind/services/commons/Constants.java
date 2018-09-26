ee3a91e016e7acf4a3be564aa65c9ec5a3a1b6e5
dangiankit <dangiankit@gmail.com>
2018-09-26 14:03:57 -0400

    add smw-commons library with annotations

---
+++ smw-commons/src/main/java/edu/cmu/inmind/services/commons/Constants.java
@@ -0,0 +1,26 @@
+package edu.cmu.inmind.services.commons;
+
+/**
+ * Created by oscarr on 8/10/18.
+ */
+public class Constants {
+
+    public static final String REQUIRES_FULLY_CHARGED = "REQUIRES_FULLY_CHARGED";
+    public static final String WORKS_WITH_LOW_CHARGE = "WORKS_WITH_LOW_CHARGE";
+    public static final String PHONE_LOW_BATTERY = "PHONE_LOW_BATTERY";
+    public static final String PHONE_HIGH_BATTERY = "PHONE_HIGH_BATTERY";
+    public static final String CHECK_BATTERY = "CHECK_BATTERY";
+
+    public static final String REQUIRES_WIFI_CONNECTIVITY = "REQUIRES_WIFI_CONNECTIVITY";
+    public static final String NOT_REQUIRES_WIFI_CONNECTIVITY = "NOT_REQUIRES_WIFI_CONNECTIVITY";
+    public static final String CHECK_WIFI = "CHECK_WIFI";
+    public static final String WIFI_ON = "WIFI_ON";
+    public static final String WIFI_OFF = "WIFI_OFF";
+
+    public static final String PICK_REMOTE_SERVICE = "PICK_REMOTE_SERVICE";
+    public static final String PICK_LOCAL_SERVICE = "PICK_LOCAL_SERVICE";
+    public static final String LOW_BATTERY_SERVICE = "LOW_BATTERY_SERVICE";
+    public static final String HIGH_BATTERY_SERVICE = "HIGH_BATTERY_SERVICE";
+
+    public static final String END = "END";
+}
