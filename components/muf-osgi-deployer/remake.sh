b9fb8682a615d89783c4f437a58c958129469ea5
dangiankit <dangiankit@gmail.com>
2018-10-09 21:47:39 -0400

    add MUF OSGi deployer component

---
+++ components/muf-osgi-deployer/remake.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+component="MUF OSGi Deployer Component.."
+
+echo "Cleaning $component"
+./clean.sh
+
+echo "Building $component"
+./make.sh
+
+echo "Running $component"
+./run.sh
+
+echo "All done."
