0f84fcf37ae4aa2440fef12422290aed8ac182af
dangiankit <dangiankit@gmail.com>
2018-10-11 22:39:24 -0400

    rm muf-middleware component -- replaced by muf-service-registry, and future: muf-service-executor component

--- components/muf-middleware/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-# java -jar ./build/libs/muf-middleware-1.0.0.jar -Xmx2048
--- components/muf-middleware/src/main/java/edu/cmu/inmind/services/muf/components/middleware/MiddlewareController.java
+++
@@ -1,6 +0,0 @@
-package edu.cmu.inmind.services.muf.components.middleware;
-
-public class MiddlewareController {
-
-}
-
